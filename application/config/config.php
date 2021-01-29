b4a7de12b17e2f60f325d5a5315cf624314b3e07
Noriel Jon Navarro <noriel@smedia.com.au>
2021-01-29 13:42:34 +0800

    updated url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://www.norieljon-portfolio.test/';
+$config['base_url'] = 'https://norieljon-portfolio.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
